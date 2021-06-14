47b0626f466f3025dd6e3e5e4a6b8843bd60c472
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-14 12:19:29 -0300

    Criação inicial do footer e header

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Ranik. Alguns direitos reservados</p>
+  </footer>
+</template>
+
+<script>
+export default {
+name:'TheFooter',
+}
+</script>
+
+<style scoped>
+ footer{
+   background: #b7f;
+   padding: 25px 0;
+   text-align: center;
+   font-size: 1.2rem;
+   color: #fff;
+   position: relative;
+   top: 75px;
+ }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/" class="logo">
+        <img src="@/assets/ranek.svg" alt="Ranik">
+      </router-link>
+      <router-link to="/login" class="btn">Vender / Comprar</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+name:'TheHeader',
+}
+</script>
+
+<style scoped>
+nav{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30,60,90,0.1);
+}
+
+.logo img{
+  width: 90px;
+}
+</style>
