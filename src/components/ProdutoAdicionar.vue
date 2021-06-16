6a83ae37f0d8ea9a9c7c16c945fd6316876b48fe
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-16 08:33:21 -0300

    Criação inicial do form e cadastro de novos produtos

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -1,18 +1,56 @@
 <template>
-  <p>Produto adicionar</p>
+  <form class="adicionar_produto">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" name="nome" v-model="produto.nome"/>
+    <label for="preco">Preço (R$)</label>
+    <input type="number" id="preco" name="preco" v-model="produto.preco"/>
+    <label for="descricao">Descrição</label>
+    <textarea id="descricao" name="descricao" v-model="produto.descricao"></textarea>
+    <label for="foto">Foto</label>
+    <input type="file" id="foto" name="foto" ref="fotos"/>
+    <input type="button" class="btn" value="Adicionar Produto" @click.prevent="adicionarProduto">
+  </form>
 </template>
 
 <script>
+import { api } from "@/services.js";
+
 export default {
 name:'ProdutoAdicionar',
 data(){
   return{
-    
+   produto:{
+     nome:'',
+     preco:'',
+     descricao:'',
+     fotos:null
+   }
+  }
+},
+methods:{
+  formatarProduto(){
+    this.produto.usuario_id = this.$store.state.usuario.id
+  },
+  adicionarProduto(){
+    this.formatarProduto()
+    api.post("/produto",this.produto)
+    .then(() => {
+      this.$store.dispatch("getUsuarioProdutos")
+    })
   }
 }
 }
 </script>
 
-<style>
+<style scoped>
+.adicionar_produto {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
 
+.btn {
+  grid-column: 2;
+}
 </style>
